8cc237677210e856aa38f9a6aca437bc3b9cf175
sqLinkList <2563185636@qq.com>
2022-03-08 21:09:20 +0800

    Week 2 Commit

--- src/main/java/com/Liuzijian/week2/demo/HelloWorldServlet.java
+++ src/main/java/com/Liuzijian/week2/demo/HelloWorldServlet.java
@@ -1,6 +1,5 @@
 package com.Liuzijian.week2.demo;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
