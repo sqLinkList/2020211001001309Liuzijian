763a94fe1221b9d811eb8baaab1d089b599b2115
sqLinkList <2563185636@qq.com>
2022-03-08 20:39:29 +0800

    Week 2 Commit

--- src/main/java/com/Liuzijian/week2/demo/HelloWorldServlet.java
+++ src/main/java/com/Liuzijian/week2/demo/HelloWorldServlet.java
@@ -1,14 +1,28 @@
 package com.Liuzijian.week2.demo;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HelloWorldServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        PrintWriter writer = response.getWriter();
-        writer.println("Hello Client !!!");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
     }
-    public void dopost(HttpServletRequest request, HttpServletResponse response){
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Name:Liuzijian");
+        writer.println("ID:2020211001001309");
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd");
+        simpleDateFormat.format(date);
+        writer.println("Date and Time"+date);
     }
 }
