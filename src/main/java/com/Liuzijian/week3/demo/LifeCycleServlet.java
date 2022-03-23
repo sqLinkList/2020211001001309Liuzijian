05f10e5df51d45cbfe2c5492e647f842ade6c3c2
sqLinkList <2563185636@qq.com>
2022-03-23 10:38:19 +0800

    Week 4 Commit

--- src/main/java/com/Liuzijian/week3/demo/LifeCycleServlet.java
+++ src/main/java/com/Liuzijian/week3/demo/LifeCycleServlet.java
@@ -4,15 +4,21 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @WebServlet(name = "LifeCycleServlet", value = "/LifeCycleServlet")
 public class LifeCycleServlet extends HttpServlet {
+
+    Connection con=null;
     public LifeCycleServlet(){
         System.out.println("i am constructor --> LifeCycleServlet");
     }
     @Override
-    public void init(){
-        System.out.println("i am in init() ");
+    public void init() {
+
+        System.out.println("i am in init()-> "+con);
     }
 
     @Override
@@ -27,5 +33,10 @@ public class LifeCycleServlet extends HttpServlet {
     @Override
     public void destroy() {
         System.out.println("i am in destroy () --> doGet()");
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
